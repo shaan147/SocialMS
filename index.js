d6e4a61af334510bae9d2d5e433565df1d04b60f
Shaan E Mustafa <shaanemustafa8@gmail.com>
2023-09-13 11:15:36 +0500

    v2

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const session = require('express-session');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const homeRoute = require('./routes/homeRoute');
+const app = express();
+const port = 3001;
+
+
+app.engine('ejs', ejsMate);
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.set(path.join(__dirname, 'views'));
+// Use sessions to persist login sessions
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define the Facebook Strategy
+passport.use(new FacebookStrategy({
+  clientID: '318689430818400',
+  clientSecret: '94735085b45d7ac23219450ffc0f7384',
+  callbackURL: 'http://localhost:3001/home'
+}, (accessToken, refreshToken, profile, done) => {
+
+  const user = { id: profile.id, displayName: profile.displayName };
+  return done(null, user);
+}));
+// Serialize user for session
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+// Deserialize user from session
+passport.deserializeUser((id, done) => {
+
+  const user = { id: id, displayName: 'User' };
+  done(null, user);
+});
+// ...
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Callback route to handle the Facebook login response
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/home' }),
+  (req, res) => {
+    // Successful authentication, redirect to a different page or respond with a message
+    res.redirect('/profile');
+  }
+);
+app.use(homeRoute);
+
+// Start the server 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ routes/homeRoute.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+router.get('/home', async (req, res) => {
+    try {
+        const { code } = req.query;
+        if (!code) {
+            return res.status(400).send('Code not provided');
+        }
+        const appId = '318689430818400';
+        const appSecret = '94735085b45d7ac23219450ffc0f7384';
+        const redirectURI = 'http://localhost:3001/home';
+        // Make a request to exchange the code for an access token
+        const response = await axios.get(`https://graph.facebook.com/v6.0/oauth/access_token`, {
+            params: {
+                client_id: appId,
+                client_secret: appSecret,
+                redirect_uri: redirectURI,
+                code: code,
+            },
+        });
+        if (response.data.access_token) {
+            // You have the user access token here
+            const userAccessToken = response.data.access_token;
+            // Log the access token to the console
+            console.log('User Access Token:', userAccessToken);
+            // You can use or store the access token as needed
+        
+ // Fetch user data using the user access token
+ const userDataResponse = await axios.get(
+    'https://graph.facebook.com/v13.0/me',
+    {
+      params: {
+        fields:
+          'id,name,email,birthday,age_range,gender,hometown,link',
+        access_token: userAccessToken,
+      },
+    }
+  );
+  const userData = userDataResponse.data;
+
+  res.render('./homepage', {
+    userData
+   
+  });
+        } else {
+            res.status(400).send('Failed to obtain user access token');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal server error');
+    }
+});
+
+module.exports = router;
+
+
